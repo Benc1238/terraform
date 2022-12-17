a822cc5c9ffa024c6916d95bb4b68f157b6a868c
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-12-17 06:49:26 -0500

    Delete azure-rm.tf

--- azure-rm.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 0.12"
-  backend "azurerm" {
-    resource_group_name  = "dataengineeringe2e"
-    storage_account_name = "dataengineeringe2e"
-    container_name       = "tfstate"
-    key                  = "state.tfstate"
-  }
-}
