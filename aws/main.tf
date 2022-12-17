a0b7f880590ff3ef90453f4d440b71ce65ac5df2
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-12-17 06:47:34 -0500

    Create main.tf

---
+++ aws/main.tf
@@ -0,0 +1,10 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create an EC2 instance running Red Hat
+resource "aws_instance" "example" {
+  ami           = "ami-08e637cea2f053dfa"
+  instance_type = "t2.micro"
+}
