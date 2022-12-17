7d525b102585a3ea77d6ffd8f067e895b205831a
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-12-17 08:01:06 -0500

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -4,12 +4,4 @@ resource "google_compute_instance" "default" {
   machine_type = "f1-micro"
   zone         = "us-east1-a"
   tags         = ["ssh"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
 }
